412b7fb9773f4fdb905801c0d76f751187b1800a
John Riviere <mr.jriviere@gmail.com>
2020-09-15 19:07:02 -0500

    got components loading on screen

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Footer</h3>
+                
+            </div>
+        )
+    }
+}
+
+
+
+export default Footer;
